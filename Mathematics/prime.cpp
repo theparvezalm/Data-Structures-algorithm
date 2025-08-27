f33ad79ff4b95ebaca8132287b5bcc75e3ee2546
parvez <parvez@Parvezs-MacBook-Air.local>
2025-08-27 21:13:47 +0530

    prime number alternate

--- Mathematics/prime.cpp
+++ Mathematics/prime.cpp
@@ -1,22 +1,35 @@
 #include<iostream>
 using namespace std;
 class Solution{
-    public:
+public:
+    // bool isPrime(int x){
+    //     if (x<=1) return false;
+    //     if (x == 2) return true;
+    //     if (x % 2 == 0 || x%3 ==0) return false;
+    //     int countFactor = 0;
+    //     int num = x;
+    //     while(num>0){
+    //         if (x%num==0){
+    //             countFactor++;
+    //         }
+    //         num --;
+    //     }
+    //     if (countFactor>2){
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    //alternate better solution
     bool isPrime(int x){
         if (x<=1) return false;
-        if (x == 2) return true; 
-        if (x % 2 == 0) return false;
-        int countFactor = 0;
-        int num = x;
-        while(num>0){
-            if (x%num==0){
-                countFactor++;
-            }
-            num --;
+        if (x == 2) return true;
+        if (x % 2 == 0 || x%3 ==0) return false;
+        int i =2;
+        while (i*i<x) {
+            if (x%i==0) return false;
+            i++;
         }
-        if (countFactor>2){
-            return false;
-        }
+
         return true;
     }
 
@@ -27,7 +40,7 @@ int main(){
     cin>>x;
     Solution sol;
     bool isPrime = sol.isPrime(x);
-   cout << "isPalindrome = " << (isPrime ? "true" : "false") << endl;
-   return 0;
+    cout << "isPalindrome = " << (isPrime ? "true" : "false") << endl;
+    return 0;
 
 }
